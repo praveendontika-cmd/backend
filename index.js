8b46fc14af1c5e330b26267d14f9a270067751ec
Praveen <praveendontika@gmail.com>
2026-02-03 16:50:44 +0530

    2nd commit

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const cors= require('cors');
 const bcrypt=require('bcrypt');
  
 const app=express();
-app.get('/',(req,res)=> 
-    {
+app.get('/',(req,res)=> {
+
+    app.use(corsI());
+
+    app.use(express.urleancoded({extended: true });)
     console.log(req)
     res.status(200).json({message:'sucessful'});
 })
+    app.post('/registeruser',(req,res)=>{
+        res.status(200).json({message:'user registered'})
+    }
 app.listen(3000,() => {
     console.log('Server started  on port 3000!');
-    console.log("hello");
+
 });
